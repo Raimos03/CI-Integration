149b82d453730836f1056a9d31aec401950ec8c2
Pedro Lima <pedrolimae3@gmail.com>
2022-08-21 11:16:27 -0300

    update

--- javascript/integration.js
+++ javascript/integration.js
@@ -3,9 +3,13 @@ function imprime(n){
     let i=0;
     for (i;i<=n;i++){
         
-        console.log(i);
+    
         if(i%2!=0){
-            console.log('${i} - numero primo' )
+            console.log(`${i} - numero primo `);
+        }
+        else{
+
+            console.log(i);
         }
 
     }
