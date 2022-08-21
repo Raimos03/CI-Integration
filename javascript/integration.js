168724aedee30fe190a6982273f85ff1e35a007a
Pedro Lima <pedrolimae3@gmail.com>
2022-08-21 10:43:55 -0300

    arquivo teste CI

---
+++ javascript/integration.js
@@ -0,0 +1,16 @@
+
+function imprime(n){
+    let i=0;
+    for (i;i<=n;i++){
+
+        console.log(i);
+    }
+
+    return;
+}
+
+
+
+//-----------main-----
+
+imprime(10);
---
+++ python/integration.py
@@ -0,0 +1,11 @@
+
+def conta(n):
+    for i in range(0,10):
+
+        print(i)
+
+    
+
+
+
+conta(10)
